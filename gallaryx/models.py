f5d488e77b43b61480b0e4d6454e2016bf719704
hope-mbuthye38 <-C>
2021-10-13 09:00:26 +0300

    all content database

--- gallaryx/models.py
+++ gallaryx/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Location(models.Model):
+   name = models.CharField(max_length=30)
+
+class Category (models.Model):
+    list = models.CharField(max_length =30)
+
+class photos (models.Model):
+    photo = models.ImageField(upload_to= 'photo/')
+    description =models.TextField(max_length=30)
+    location= models.ForeignKey(Location,on_delete=models.CASCADE)
+    Category = models.TextField(max_length=30)
+
+    
